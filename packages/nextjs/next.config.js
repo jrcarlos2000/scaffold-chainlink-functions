3ec414ce397dbcf9c8ae659948a8edba76e55674
ngyixuan <ngyixuan2009@gmail.com>
2023-08-04 02:20:53 +0800

    fix: fix configuration

--- packages/nextjs/next.config.js
+++ packages/nextjs/next.config.js
@@ -4,7 +4,7 @@
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
-    ignoreBuildErrors: true,
+    ignoreBuildErrors: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
   },
   eslint: {
     ignoreDuringBuilds: process.env.NEXT_PUBLIC_IGNORE_BUILD_ERROR === "true",
